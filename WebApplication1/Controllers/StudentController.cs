acd70fd2822824a48bed7b6e44636b4c465fdcfb
khalillahori <khalillahori@gmail.com>
2023-10-12 16:22:47 +0500

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[Controller.cs]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+//    [HttpGet("GetcarcompanyName")]
+    
+      // List <stuent> stuents = new List<stuent> get()
+        public IActionResult abc()
+        {
+            return ok();
+        }
+    }
+}
